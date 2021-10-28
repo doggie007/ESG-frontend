ef6e22c7964787bb4b65e95d095fa3073c187313
James Tcheng <james.tcheng1234@gmail.com>
2021-10-28 23:00:10 +0100

    Ready for heroku deployment

--- src/api/axios-base.js
+++ src/api/axios-base.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const APIUrl = "http://127.0.0.1:8000/api/";
+const APIUrl = "https://backend-investures.herokuapp.com/api/";
 const axiosBase = axios.create({
 	baseURL: APIUrl,
 	// headers: { contentType: "application/json" },
